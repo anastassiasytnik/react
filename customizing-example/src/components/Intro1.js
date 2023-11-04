f4c90c744105ca6e8acd8f0d8c12a1e11563ed59
Anastassia Sytnik <39063293+anastassiasytnik@users.noreply.github.com>
2023-11-03 19:11:46 -0700

    Filled out components, but not using them yet

---
+++ customizing-example/src/components/Intro1.js
@@ -0,0 +1,14 @@
+function Intro1() {
+    return (
+        <div className="blog-post-intro">
+            <h2>I've become a React developer!</h2>
+            <div>
+                <p>I've completed the React Basics course and I am happy to announce
+                   that I am now a Junior React Developer!</p>
+                <p className="link">Read more...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Intro1;
